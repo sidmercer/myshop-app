8000d6aefee7ac732088c8e588acff330cae9e04
siddhant kamble <69417433+sidmercer@users.noreply.github.com>
2024-04-28 12:25:53 +0530

    Add files via upload

---
+++ src/components/ProductListing.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import ProductComponent from './ProductComponent';
+import {fetchProducts, setProducts} from '../redux/actions/productActions';
+
+export default function ProductListing(props) {
+    const data = useSelector(state => state.allProducts)
+    // console.log(data)
+    const dispatch=useDispatch()
+
+    // const loadProducts = async () => {
+    //   const response = await axios.get('https://fakestoreapi.com/products')
+    //   // console.log(response);
+    //   dispatch(setProducts(response.data))
+    // }
+    useEffect(()=>{
+      dispatch(fetchProducts())
+    },[])
+      console.log(data);
+  return (
+    <div className='ui grid container'>
+        {/* <h3>Product Listing</h3> */}
+        <ProductComponent/>
+    </div>
+  )
+}
---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,32 @@
+import { ActionTypes } from "../constants/action-types"
+import axios from "axios"
+import fakeStore from "../../apis/fakeStore"
+
+export const fetchProduct=(id)=>async(dispatch)=>{        //this
+     
+    const response=await fakeStore.get(`/products/${id}`)
+    dispatch({type:ActionTypes.SELECTED_PRODUCTS,payload:response.data})
+
+}
+
+
+export const fetchProducts=()=>async(dispatch)=>{
+     
+         const response=await fakeStore.get('/products')
+         dispatch({type:ActionTypes.FETCH_PRODUCTS,payload:response.data})
+     
+}
+
+export const setProducts = (products) => {
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: products,
+  };
+};
+
+export const selectedProduct = (product) => {
+  return {
+    type: ActionTypes.SELECTED_PRODUCTS,
+    payload: product,
+  };
+};
